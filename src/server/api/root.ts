a297a6a78514857beb2126ddef2096788aa56fe6
Martoxdlol <113550464+EscuelaTecnicaHenryFord@users.noreply.github.com>
2023-05-12 15:52:06 -0300

    api

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
-import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,56 @@ import { exampleRouter } from "~/server/api/routers/example";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  getBooks: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.book.findMany();
+  }),
+  addBook: protectedProcedure.input(z.object({
+    code: z.number(),
+    title: z.string(),
+    author: z.string(),
+    genre: z.string(),
+    editor: z.string(),
+  })).mutation(({ ctx, input }) => {
+    return ctx.prisma.book.create({
+      data: {
+        code: input.code,
+        title: input.title,
+        author: input.author,
+        genre: input.genre,
+        editor: input.editor,
+      }
+    })
+  }),
+  updateBook: protectedProcedure.input(z.object({
+    id: z.string(),
+    code: z.number(),
+    title: z.string(),
+    author: z.string(),
+    genre: z.string(),
+    editor: z.string(),
+  })).mutation(({ ctx, input }) => {
+    return ctx.prisma.book.update({
+      where: {
+        id: input.id
+      },
+      data: {
+        code: input.code,
+        title: input.title,
+        author: input.author,
+        genre: input.genre,
+        editor: input.editor,
+      }
+    })
+  }),
+  deleteBook: protectedProcedure.input(z.object({
+    id: z.string(),
+  })).mutation(({ ctx, input }) => {
+    return ctx.prisma.book.delete({
+      where: {
+        id: input.id
+      }
+    })
+  })
 });
 
 // export type definition of API
